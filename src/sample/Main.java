89b154c5b21cdcc8e1bab0c87d8bbe924654b910
DESKTOP-P35EP4G\kidpo <kidpooh2k@gmail.com>
2020-10-19 21:37:53 +0700

    update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Dictionary");
-        primaryStage.setScene(new Scene(root, 600, 560));
+        primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
 
     }
