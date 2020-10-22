3b235b080ec86c2c64738d568209043e008232df
LAPTOP-1DM99TID\ASUS <lethianwinwin@gmail.com>
2020-10-22 10:33:37 +0700

    api

---
+++ src/sample/Controller.java
@@ -0,0 +1,80 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    public void ChangeScene2(ActionEvent event) throws IOException {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+
+        //This line gets the Stage information
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(tableViewScene);
+        window.show();
+    }
+
+    @FXML
+    public Button changeSceneButton2;
+    @FXML
+    public Button searchButton;
+    @FXML
+    public TextArea input;
+    @FXML
+    public TextArea output;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)  {
+
+        searchButton.setOnMouseClicked(mouseEvent -> {
+            try {
+                output.setText(translate("en","vi",input.getText()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+    }
+
+
+    private static String translate(String langFrom, String langTo, String text) throws IOException {
+        // INSERT YOU URL HERE
+        String urlStr = "https://script.google.com/macros/s/AKfycbzuAVrY273vC30HuSNOGefuuAvCWVEfFWlF_9PitWytSwegno-e/exec" +
+                "?q=" + URLEncoder.encode(text, "UTF-8") +
+                "&target=" + langTo +
+                "&source=" + langFrom;
+        URL url = new URL(urlStr);
+        StringBuilder response = new StringBuilder();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+}
